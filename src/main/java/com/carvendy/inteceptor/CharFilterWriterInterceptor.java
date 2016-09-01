5b707a09ab3f7074c221b197cd36742c799e95d2
13247677120 <429659162@qq.com>
2016-09-01 14:09:11 +0800

    code update

--- src/main/java/com/carvendy/inteceptor/CharFilterWriterInterceptor.java
+++ src/main/java/com/carvendy/inteceptor/CharFilterWriterInterceptor.java
@@ -1,65 +1,30 @@
 package com.carvendy.inteceptor;
 
-import java.io.IOException;
+import java.io.OutputStream;
 
-import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
-import org.apache.cxf.staxutils.DelegatingXMLStreamWriter;
+import org.apache.cxf.interceptor.AttachmentOutInterceptor;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.staxutils.StaxUtils;
 
-import com.ctc.wstx.api.InvalidCharHandler;
+import com.youboy.esb.interceptor.util.CharFilterXMLStreamWriter;
 
-public class CharFilterXMLStreamWriter extends DelegatingXMLStreamWriter { 
+public class CharFilterWriterInterceptor extends AbstractPhaseInterceptor<Message> { 
 
-        private String currentElementName; 
-        
-        private static String regex = "";
-        
-        static{
-        	for(int i=0; i<32; i++){
-        		regex += ((char)i);
-        	}
-        	regex = "["+regex+"]";
-        }
-
-        public CharFilterXMLStreamWriter(XMLStreamWriter del) { 
-                super(del); 
+        public CharFilterWriterInterceptor() { 
+                super(Phase.PRE_STREAM); 
+                addAfter(AttachmentOutInterceptor.class.getName()); 
         } 
 
         @Override 
-        public void writeCharacters(String text) throws XMLStreamException { 
-        	/*String tmp = text;
-        	for(char c :text.toCharArray()){
-        		if(c < 0x0020){
-        			tmp = tmp.replaceAll(c+"", " ");
-        		}
-        	}
-        	text = tmp;*/
-        	text = text.replaceAll(regex, " ");
-        	super.writeCharacters(text); 
+        public void handleMessage(Message message) { 
+                message.put("disable.outputstream.optimization", Boolean.TRUE); 
+                XMLStreamWriter writer = 
+                		StaxUtils.createXMLStreamWriter(message.getContent(OutputStream.class)); 
+                message.setContent(XMLStreamWriter.class, new CharFilterXMLStreamWriter(writer)); 
         } 
-        
-        public boolean filterChar(char c){
-        	InvalidCharHandler failingHandler = InvalidCharHandler.FailingHandler.getInstance();
-        	try {
-				failingHandler.convertInvalidChar(c);
-			} catch (IOException e) {
-				return true;
-			}
-        	
-        	return false;
-        }
-
-        public void writeStartElement(String prefix, String local, String uri) throws XMLStreamException { 
-                currentElementName = local; 
-                super.writeStartElement(prefix, local, uri); 
-        } 
-        
-        
-        public static void main(String[] args) {
-			String text = "00123456789a/\\非法字符////118079";
-			System.out.println("regex:"+regex);
-        	text = text.replaceAll(regex, " ");
-        	System.out.println(text);
-		}
 } 
+
--- src/main/java/com/carvendy/inteceptor/CharFilterXMLStreamWriter.java
+++ src/main/java/com/carvendy/inteceptor/CharFilterXMLStreamWriter.java
@@ -8,11 +8,19 @@ import javax.xml.stream.XMLStreamWriter;
 import org.apache.cxf.staxutils.DelegatingXMLStreamWriter;
 
 import com.ctc.wstx.api.InvalidCharHandler;
-import com.ctc.wstx.api.InvalidCharHandler.FailingHandler;
 
 public class CharFilterXMLStreamWriter extends DelegatingXMLStreamWriter { 
 
         private String currentElementName; 
+        
+        private static String regex = "";
+        
+        static{
+        	for(int i=0; i<32; i++){
+        		regex += ((char)i);
+        	}
+        	regex = "["+regex+"]";
+        }
 
         public CharFilterXMLStreamWriter(XMLStreamWriter del) { 
                 super(del); 
@@ -20,13 +28,14 @@ public class CharFilterXMLStreamWriter extends DelegatingXMLStreamWriter {
 
         @Override 
         public void writeCharacters(String text) throws XMLStreamException { 
-        	String tmp = text;
+        	/*String tmp = text;
         	for(char c :text.toCharArray()){
         		if(c < 0x0020){
         			tmp = tmp.replaceAll(c+"", " ");
         		}
         	}
-        	text = tmp;
+        	text = tmp;*/
+        	text = text.replaceAll(regex, " ");
         	super.writeCharacters(text); 
         } 
         
@@ -45,4 +54,12 @@ public class CharFilterXMLStreamWriter extends DelegatingXMLStreamWriter {
                 currentElementName = local; 
                 super.writeStartElement(prefix, local, uri); 
         } 
+        
+        
+        public static void main(String[] args) {
+			String text = "00123456789a/\\非法字符////118079";
+			System.out.println("regex:"+regex);
+        	text = text.replaceAll(regex, " ");
+        	System.out.println(text);
+		}
 } 
